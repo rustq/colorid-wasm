30ffe0b3f87c3d969cd1edd6bb57d395dd6a702f
meloalright <meloalright@gmail.com>
2022-09-13 22:34:44 +0800

    Debug for format of 0.0.2

--- src/format.ts
+++ src/format.ts
@@ -8,12 +8,11 @@ export const HEX: string[] = [];
 
 function format(count: number, random: (size: number) => Uint8Array) {
   const buf = random(count * 3);
-  let str = "";
+  let str = "#";
   for (let i = 0; i < count; i++) {
-    const color = "#" + HEX[buf[i]] + HEX[buf[i + 1]] + HEX[buf[i + 2]];
-    str += color;
+    str += HEX[buf[i]] + HEX[buf[i + 1]] + HEX[buf[i + 2]];
     if (i !== count - 1) {
-      str += "-";
+      str += "-#";
     }
   }
   return str;
